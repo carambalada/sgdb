24e114185fae95b528fb04d0828bfd525c374ae2
carambalada <timur@xrobo.ru>
2016-08-14 20:27:11 +0300

    combining sql and ldap

--- sgdb.py
+++ sgdb.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 # A config-file should be passed as a parameter to this script.
 # The content of the config-file should be something like this:
@@ -11,26 +12,36 @@
 # name = myname
 # --------------------
 
-import sys
-import MySQLdb
+import os, sys, MySQLdb, ldap
 from ConfigParser import SafeConfigParser
 
-""" ini
+#
+# Getting options from ini-file
+#
+def fGetOptions( inifile ):
+ body = file(inifile, 'rt').read()
+
+ parser = SafeConfigParser()
+ parser.read(inifile)
+
+ opt = {}
+ opt['host'] = parser.get('db','host')
+ opt['user'] = parser.get('db','user')
+ opt['pass'] = parser.get('db','pass')
+ opt['name'] = parser.get('db','name')
+
+ return opt
+
+#
+# Main
+#
 if len(sys.argv) != 2:
  print 'Please specify one inifile on the command line.'
  sys.exit(1)
+else:
+ opt = fGetOptions( sys.argv[1] )
 
-inifile = sys.argv[1]
-body = file(inifile, 'rt').read()
-
-parser = SafeConfigParser()
-parser.read(inifile)
-
-dbhost = parser.get('db','host')
-dbuser = parser.get('db','user')
-dbpass = parser.get('db','pass')
-dbname = parser.get('db','name')
-"""
+print opt['host']
 
 """ sql
 db = MySQLdb.connect("dbhost","dbuser","dbpass","dbname")
@@ -50,4 +61,48 @@ except:
  print "Error"
 
 db.close()
+"""
+
+""" ldap
+
+l = ldap.initialize('ldap://')
+binddn = ""
+pw = ""
+basedn = ""
+
+searchFilter = "(CN=Сервис.*)"
+searchAttribute = ["postalcode","postofficebox"]
+#this will scope the entire subtree under UserUnits
+searchScope = ldap.SCOPE_SUBTREE
+#Bind to the server
+try:
+    l.protocol_version = ldap.VERSION3
+    l.simple_bind_s(binddn, pw) 
+except ldap.INVALID_CREDENTIALS:
+  print "Your username or password is incorrect."
+  sys.exit(0)
+except ldap.LDAPError, e:
+  if type(e.message) == dict and e.message.has_key('desc'):
+      print e.message['desc']
+  else: 
+      print e
+  sys.exit(0)
+
+try:    
+ ldap_result_id = l.search(basedn, searchScope, searchFilter, searchAttribute, attrsonly=0)
+ result_set = []
+ while 1:
+  result_type, result_data = l.result(ldap_result_id, 0)
+  if (result_data == []):
+   break
+  else:
+   if result_type == ldap.RES_SEARCH_ENTRY:
+    result_set.append(result_data)
+ print "result: ", result_set[0][0][1]['postalCode'][0]
+ print "result: ", result_set[0][0][1]['postOfficeBox'][0]
+ 
+except ldap.LDAPError, e:
+    print e
+
+l.unbind_s()
 """
