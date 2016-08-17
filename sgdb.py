a0e38567b59114ba342a27905499dfcf2787be37
carambalada <timur@xrobo.ru>
2016-08-17 16:36:02 +0300

    getting options from ini file improvments

--- sgdb.py
+++ sgdb.py
@@ -13,34 +13,31 @@
 # --------------------
 
 import os, sys, MySQLdb, ldap
-from ConfigParser import SafeConfigParser
+import ConfigParser
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #
 # Getting options from ini-file
 #
-def fGetOptions( inifile ):
- body = file(inifile, 'rt').read()
-
- parser = SafeConfigParser()
+def get_options(section, inifile):
+ parser = ConfigParser.SafeConfigParser()
  parser.read(inifile)
 
- opt = {}
- opt['host'] = parser.get('db','host')
- opt['user'] = parser.get('db','user')
- opt['pass'] = parser.get('db','pass')
- opt['name'] = parser.get('db','name')
+ options = {}
 
- return opt
+ for couple in parser.items(section):
+  options[couple[0]] = parser.get(section, couple[0])
+
+ return options
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #
 # Getting a row from DB
 #
-def fSQLGetRow( db, st ):
+def sql_get_all(db, st):
  cursor = db.cursor()
 
  try:
-  cursor.execute( st )
+  cursor.execute(st)
   results = cursor.fetchall()
  except:
   print "Error"
@@ -48,33 +45,61 @@ def fSQLGetRow( db, st ):
  return results
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #
+# Getting domain tail
+#
+def get_domin_tail(db, table, domain_id):
+ cursor = db.cursor()
+
+ try:
+  cursor.execute("SELECT name from %s WHERE id=%d" % (table, domain_id))
+  results = cursor.fetchone()
+ except:
+  print "Error"
+
+ return results
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+# Getting domain names
+#
+# db = db link
+# table: table name
+# root (bool): include root (first-level) domain names?, i.e. 0 would include "com" in return list
+#
+def get_domain_names(db, table, root):
+ rows = sql_get_all(db, "SELECT * FROM %s" % table)
+ for row in rows:
+  print "domain: %s%s" % (row[1], get_domain_tail(db, table, row[2]))
+  
+ return 
+ 
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
 # Main
 #
-"""
 if len(sys.argv) != 2:
  print 'Please specify one inifile on the command line.'
  sys.exit(1)
 
-opt = fGetOptions( sys.argv[1] )
-db = MySQLdb.connect(opt['host'],opt['user'],opt['pass'],opt['name'])
+dbopt = get_options('db', sys.argv[1])
+db = MySQLdb.connect(dbopt['host'],dbopt['user'],dbopt['pass'],dbopt['name'])
 
-dmn = fSQLGetRow( db, "SELECT * FROM dmn" )
-print dmn
+domains = get_domain_names(db, 'domain', 1)
+
+""" ldap
 #for row in dmn:
 # print "domain: ", row[0], row[1]
 
 db.close()
-"""
 
-""" ldap
-"""
 
-l = ldap.initialize('ldap://kl.com')
+l = ldap.initialize('ldap://shops.kl.com')
 user = "ldap_query@kl.com"
 pw = "1q2w3e4r5t"
-basedn = "OU=Users (kl.com),DC=kl,DC=com"
+basedn = "OU=Access groups,DC=kl,DC=com"
+basedn = "OU=Access groups,DC=shops,DC=kl,DC=com"
 
-searchFilter = "(CN=Сервис.*)"
+searchFilter = "(CN=3*)"
 searchAttribute = ["postalcode","postofficebox"]
 #this will scope the entire subtree under UserUnits
 searchScope = ldap.SCOPE_SUBTREE
@@ -102,10 +127,10 @@ try:
   else:
    if result_type == ldap.RES_SEARCH_ENTRY:
     result_set.append(result_data)
- print "result: ", result_set[0][0][1]['postalCode'][0]
- print "result: ", result_set[0][0][1]['postOfficeBox'][0]
+ print "result: ", result_set[0]
  
 except ldap.LDAPError, e:
     print e
 
 l.unbind_s()
+"""
